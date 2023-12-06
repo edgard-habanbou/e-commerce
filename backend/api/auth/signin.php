9acb26b730af52ae24f38ca29f80d5419911dac9
edgard <edgard.habanbou@hotmail.com>
2023-12-07 01:13:50 +0200

    Added signup

--- backend/api/auth/signin.php
+++ backend/api/auth/signin.php
@@ -11,35 +11,34 @@ header('Access-Control-Allow-Headers: Content-Type');
 include('../../config/connection.php');
 
 
-$user_email = $_POST['user_email'];
-$user_password = $_POST['user_password'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 $query = $con->prepare('SELECT user_id, password, user_role FROM users WHERE email = ?');
-$query->bind_param('s', $user_email);
+$query->bind_param('s', $email);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows;
 $query->bind_result($user_id, $hashed_password, $user_role);
 $query->fetch();
-
 $response = [];
 if ($num_rows == 0) {
     $response['status']  = 'User Not Found';
     echo json_encode($response);
 } else {
 
-    if (password_verify($user_password, $hashed_password)) {
+    if (password_verify($password, $hashed_password,)) {
 
         $secret_Key  = 'Q2FuJ3Qgd2FpdCB0byBmaW5pc2ggdGhpcyBwcm9ncmFtLg==';
         $request_data = [
             'user_id' => $user_id,
-            'role_id' => $role_id,
+            'user_role' => $user_role,
         ];
 
         $jwt = JWT::encode($request_data, $secret_Key, 'HS512');
         $response['jwt'] = $jwt;
         $response['status']  = 'Login Success';
-        $response['role_id'] = $role_id;
+        $response['user_role'] = $user_role;
 
         header('Content-Type: application/json');
         echo json_encode($response);
---
+++ backend/api/auth/signup.php
@@ -0,0 +1,37 @@
+<?php
+
+
+include('../../config/connection.php');
+
+
+
+if ($_POST['action'] == "signup") {
+
+
+    $username = $_POST['username'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $gender = $_POST['gender']; //0 Male, 1 Female
+    $role = 2; // 1 Admin 2 User
+
+    $hashed_password = crypt($password, PASSWORD_DEFAULT);
+
+    $query = $con->prepare('INSERT INTO users (username, fname, lname, user_role, email, gender, password) VALUES (?, ?, ?, ?, ?, ?, ?)');
+    $query->bind_param('sssisis', $username, $fname, $lname, $role, $email, $gender, $hashed_password);
+    $query->execute();
+
+    $response = [];
+    if ($query->error) {
+        $response['status'] = false;
+        $response['message'] = 'Error updating user: ' . $query->error;
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } else {
+        $response['status'] = true;
+        $response['message'] = 'User Created successfully';
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+}
